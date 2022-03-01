44a4e441b2ade8249ed3c4027057721a815a5cb3
AbuZobayed <zobayed.ict05@gmail.com>
2022-03-01 13:45:08 +0600

    details btn clicked

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const loadPhoneGallery = () => {
     const searchBox = document.getElementById('search-box').value;
     if(searchBox == ''){
-        const crrorShow = document.getElementById('errormasg')
+        const crrorShow = document.getElementById('error-massages')
         crrorShow.innerText='Not Fount'
     }
 
@@ -10,40 +10,51 @@ const loadPhoneGallery = () => {
 
         fetch(url)
         .then(res => res.json())
-        .then(data => displayPhones(data.data))
+        .then(data => displayPhones(data))
+
+        const crrorShow = document.getElementById('error-massages')
+        crrorShow.innerText='';
     }
 
-    
 };
 
 const displayPhones = (phones) =>{
-    if(phones.lenght == 0){
-         const errormag =document.getElementById('errormasg2');
-         errormag.innerText = ' phone is not available'
+    let phoneInfo = phones.data.slice(0,20);
+    if(phoneInfo.lenght == 0){
+        const crrorShow = document.getElementById('error-massages')
+        crrorShow.innerText=`Not Fount`
     }
-    const parentCard = document.getElementById('card-container');
-    parentCard.textContent = ''
-    for(const phone of phones){
-        
-        const div = document.createElement('div');
-       
-        div.innerHTML = `
-        <div class="col">
-              <div class="card h-100">
-                <img src="${phone.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Phone Name:${phone.phone_name}</h5>
-                  <p class="card-text">Brand:${phone.brand}</p>
-                </div>
-                
-                <button type="button" class="btn btn-primary ">More Details</button>
-                  
 
-                
-              </div>
-            </div>
-        `;
-        parentCard.appendChild(div);
-    };
+    else{
+        const parentCard = document.getElementById('card-container');
+        parentCard.textContent = '';
+        for(const phone of phoneInfo){
+        
+            const div = document.createElement('div');
+           
+            div.innerHTML = `
+            <div class="col">
+                  <div class="card h-100">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Phone Name:${phone.phone_name}</h5>
+                      <p class="card-text">Brand:${phone.brand}</p>
+                    </div>
+                    
+                    <button  onclick="phoneDetalis('${phone.slug}')"  type="button" class="btn btn-primary ">More Details</button>
+                      
+    
+                    
+                  </div>
+                </div>
+            `;
+            parentCard.appendChild(div);
+        };
+    }
     
 }
+
+const phoneDetalis = info => {
+    console.log(info);
+}
+
